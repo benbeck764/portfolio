59e987f03aa50f0f87172e8b24049df5d489b3c5
Ben Beck <ben.beck.764@gmail.com>
2023-09-28 12:24:01 -0500

    Configure Home routing

--- app/src/routing/features/SiteRouting.tsx
+++ app/src/routing/features/SiteRouting.tsx
@@ -1,14 +1,16 @@
-import { Suspense } from 'react';
-import { Outlet, RouteProps } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import { RouteProps } from 'react-router-dom';
 import { AppRoutes, RouteName } from '../common/routes';
 import { AppPageLoader } from '@benbeck764/react-components';
+
+const Home = lazy(() => import('../../features/Home/Home'));
 
 export const getSiteRoutes = (): RouteProps[] => [
   {
     path: AppRoutes[RouteName.Site].path,
     element: (
       <Suspense fallback={<AppPageLoader />}>
-        <Outlet />
+        <Home />
       </Suspense>
     ),
   },
