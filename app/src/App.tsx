c3cb09b30df571e23a09818bd2abd080694b096e
Ben Beck <ben.beck.764@gmail.com>
2023-10-12 12:48:46 -0500

    Reducing framer-motion bundle size

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppContent from './AppContent';
-import { getTheme } from './theme/theme';
 import { CustomThemeProvider } from '@benbeck764/react-components';
 import CssBaseline from '@mui/material/CssBaseline';
+import { LazyMotion, domAnimation } from 'framer-motion';
+import AppContent from './AppContent';
+import { getTheme } from './theme/theme';
 
 const theme = getTheme();
 
 function App(): JSX.Element {
   return (
     <CustomThemeProvider theme={theme}>
-      <Router>
-        <CssBaseline />
-        <AppContent />
-      </Router>
+      <LazyMotion features={domAnimation} strict>
+        <Router>
+          <CssBaseline />
+          <AppContent />
+        </Router>
+      </LazyMotion>
     </CustomThemeProvider>
   );
 }
