79a2b9163a51f4cb591d903ca507982fea213fbf
Ben Beck <ben.beck.764@gmail.com>
2023-10-17 13:06:22 -0500

    Remove spinner

--- app/src/routing/features/SiteRouting.tsx
+++ app/src/routing/features/SiteRouting.tsx
@@ -1,7 +1,6 @@
 import { Suspense, lazy } from 'react';
 import { RouteProps } from 'react-router-dom';
 import { AppRoutes, RouteName } from '../common/routes';
-import { AppPageLoader } from '@benbeck764/react-components';
 
 const Home = lazy(() => import('../../features/Home/Home'));
 
@@ -9,7 +8,7 @@ export const getSiteRoutes = (): RouteProps[] => [
   {
     path: AppRoutes[RouteName.Site].path,
     element: (
-      <Suspense fallback={<AppPageLoader />}>
+      <Suspense>
         <Home />
       </Suspense>
     ),
